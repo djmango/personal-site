a9fe318a187c3c065fa3e587261a0ec4fd63f4e1
Sulaiman Ghori <skghori03@gmail.com>
2019-04-23 21:20:42 -0400

    pls travy

--- personalWebsiteFlask/settings.py
+++ personalWebsiteFlask/settings.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from pathlib import Path  # python3 only
 from os import getenv
+from dotenv import load_dotenv
+
 """Application configuration.
 
 Most configuration is set via environment variables.
@@ -8,24 +10,21 @@ Most configuration is set via environment variables.
 For local development, use a .env file to set
 environment variables.
 """
-from environs import Env
-
 # `path.parents[1]` is the same as `path.parent.parent`
 d = Path(__file__).resolve().parents[1]
 
-env = Env()
-env.read_env(d.joinpath('pub.env'))
-env.read_env(d.joinpath('sec.env'))
+load_dotenv(d.joinpath('pub.env'))
+load_dotenv(d.joinpath('sec.env'))
 
 
-ENV = env.str('FLASK_ENV', default='production')
+ENV = getenv('FLASK_ENV', default='production')
 DEBUG = ENV == 'development'
-SQLALCHEMY_DATABASE_URI = env.str('DATABASE_URL')
+SQLALCHEMY_DATABASE_URI = getenv('DATABASE_URL')
 if getenv('SECRET_KEY') is None:
     SECRET_KEY = 'not-so-secret'
 else:
-    SECRET_KEY = env.str('SECRET_KEY')
-BCRYPT_LOG_ROUNDS = env.int('BCRYPT_LOG_ROUNDS', default=13)
+    SECRET_KEY = getenv('SECRET_KEY')
+BCRYPT_LOG_ROUNDS = getenv('BCRYPT_LOG_ROUNDS', default=13)
 DEBUG_TB_ENABLED = DEBUG
 DEBUG_TB_INTERCEPT_REDIRECTS = False
 CACHE_TYPE = 'simple'  # Can be "memcached", "redis", etc.
